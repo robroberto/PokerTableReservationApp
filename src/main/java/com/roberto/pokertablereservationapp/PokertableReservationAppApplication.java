ba088bc285c8636be1625d6975c9196335620bf3
Robin Roth <robin.roth@students.fhnw.ch>
2023-04-29 20:04:58 +0200

    Create spring app

---
+++ src/main/java/com/roberto/pokertablereservationapp/PokertableReservationAppApplication.java
@@ -0,0 +1,14 @@
+package com.roberto.pokertablereservationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PokertableReservationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokertableReservationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roberto/pokertablereservationapp/PokertableReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.pokertablereservationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokertableReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
